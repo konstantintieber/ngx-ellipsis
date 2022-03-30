6fbda5f40a9958f9b89c1b8d6a60d8b71af30325
konstantintieber <konstantintieber@users.noreply.github.com>
2022-03-30 17:07:42 +0000

    deploy: f7abcc9bc5dbecb4ee5679924712d4fd5c53257e

--- projects/ngx-ellipsis/jest.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts']
-};
--- projects/ngx-ellipsis/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-ellipsis
- */
-
-export * from './lib/lx-ellipsis.component';
-export * from './lib/ngx-ellipsis.module';
-
