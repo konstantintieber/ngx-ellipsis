8d5484834cb272d4d795144880f7ee420df528ab
Konstantin Tieber <konstantin.tieber@leanix.net>
2022-03-30 17:50:52 +0200

    Switch from Karma to Jest test runner

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts']
+};
--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { LxEllipsisModule } from '../../../ngx-ellipsis/src/lib/ngx-ellipsis.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -7,6 +8,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [LxEllipsisModule]
     }).compileComponents();
   });
 
